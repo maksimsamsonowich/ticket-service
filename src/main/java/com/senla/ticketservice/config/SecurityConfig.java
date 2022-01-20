dfd677bdca5df0647948062a21b176e1c8897979
Maxim Samsonowich <65091574+MaximSamsonowich@users.noreply.github.com>
2022-01-20 18:26:05 +0300

    some updates

---
+++ src/main/java/com/senla/ticketservice/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.senla.ticketservice.config;
+
+import com.senla.ticketservice.security.jwt.token.JwtTokenFilter;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true
+)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String AUTH_ENDPOINT = "/auth";
+    private static final String REGISTRATION_ENDPOINT = "/register";
+    private static final String GET_EVENT_ENDPOINT = "/event-management/**";
+
+    private final JwtTokenFilter jwtTokenFilter;
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, AUTH_ENDPOINT).permitAll()
+                .antMatchers(HttpMethod.POST, REGISTRATION_ENDPOINT).permitAll()
+                .antMatchers(HttpMethod.GET, GET_EVENT_ENDPOINT).permitAll()
+                .anyRequest().authenticated();
+    }
+
+}
